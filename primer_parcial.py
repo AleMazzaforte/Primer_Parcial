da88b63d4f94b85957e62a5e5b6725254ed42c3d
AleMazzaforte <109003400+AleMazzaforte@users.noreply.github.com>
2026-04-08 00:02:02 -0300

    Commit 3
    hecho hasta el punto 6

--- primer_parcial.py
+++ primer_parcial.py
@@ -5,8 +5,8 @@ print("Bienvenido al sistema de control de stock Ferreteria 'El Cosito' ")
 
 # Declaración de variables
 
-herramientas = []
-existencias = []
+herramientas = ["a", "b", "c"]
+existencias = [1, 0, 0]
 
 # Comienza el programa mostrando el menu y solicitando seleccion de actividad a realizar
 
@@ -134,13 +134,160 @@ while accion != 10:
                     """).strip()
 
         case "3":
-            pass
+            print("Stock total")
+            for i in range(len(herramientas)):
+                print(f"{herramientas[i]} - {existencias[i]}")
+             # Vuelvo a solicitar elección de menu 
+            accion = input("""
+                Ingrese un número para elegir la actividad a realizar.
+                    1. Carga de herramientas.
+                    2. Carga de existencias.
+                    3. Visualizar el inventario.
+                    4. Consulta de stock por producto.
+                    5. Reporte de quiebre de stock.
+                    6. Alta de nuevo producto.
+                    7. Actualizacion de stock.
+                        Venta o compra.
+                    10. Para salir del programa. 
+                """).strip()
+                # Vuelvo a validar
+            while not accion.isdigit() and accion not in ["1", "2", "3", "4", "5", "6", "7", "10"]:
+                print("El valor ingresado es incorrecto. Por favor ingrese solamente un número del menú")
+                accion = input("""
+                    Ingrese un número para elegir la actividad a realizar.
+                        1. Carga de herramientas.
+                        2. Carga de existencias.
+                        3. Visualizar el inventario.
+                        4. Consulta de stock por producto.
+                        5. Reporte de quiebre de stock.
+                        6. Alta de nuevo producto.
+                        7. Actualizacion de stock.
+                            Venta o compra.
+                        10. Para salir del programa. 
+                    """).strip()
         case "4":
-            pass
+            print("Stock por producto")
+            herramienta_unidad = input("Ingrese la herramiente ").strip().lower()
+                # Aca valido la herramienta 
+            while not herramienta_unidad.isalpha() or herramienta_unidad not in herramientas:
+                print("Valor incorrecto. Ingrese solo letras y asegúrese que la herramienta ya esté ingresada")
+                herramienta = input("Ingrese la herramienta ").strip().lower()
+            indice = herramientas.index(herramienta_unidad)
+            print(f"Hay {existencias[indice]} unidades de {herramientas[indice]}")
+             # Vuelvo a solicitar elección de menu 
+            accion = input("""
+                Ingrese un número para elegir la actividad a realizar.
+                    1. Carga de herramientas.
+                    2. Carga de existencias.
+                    3. Visualizar el inventario.
+                    4. Consulta de stock por producto.
+                    5. Reporte de quiebre de stock.
+                    6. Alta de nuevo producto.
+                    7. Actualizacion de stock.
+                        Venta o compra.
+                    10. Para salir del programa. 
+                """).strip()
+                # Vuelvo a validar
+            while not accion.isdigit() and accion not in ["1", "2", "3", "4", "5", "6", "7", "10"]:
+                print("El valor ingresado es incorrecto. Por favor ingrese solamente un número del menú")
+                accion = input("""
+                    Ingrese un número para elegir la actividad a realizar.
+                        1. Carga de herramientas.
+                        2. Carga de existencias.
+                        3. Visualizar el inventario.
+                        4. Consulta de stock por producto.
+                        5. Reporte de quiebre de stock.
+                        6. Alta de nuevo producto.
+                        7. Actualizacion de stock.
+                            Venta o compra.
+                        10. Para salir del programa. 
+                    """).strip()
         case "5":
-            pass
+            # Listar productos con existencia 0
+            print("Los pruductos que no hay en existencia son:")
+            for i in range(len(herramientas)):
+                if existencias[i] == 0:
+                    print(f"{herramientas[i]}")
+             # Vuelvo a solicitar elección de menu 
+            accion = input("""
+                Ingrese un número para elegir la actividad a realizar.
+                    1. Carga de herramientas.
+                    2. Carga de existencias.
+                    3. Visualizar el inventario.
+                    4. Consulta de stock por producto.
+                    5. Reporte de quiebre de stock.
+                    6. Alta de nuevo producto.
+                    7. Actualizacion de stock.
+                        Venta o compra.
+                    10. Para salir del programa. 
+                """).strip()
+                # Vuelvo a validar
+            while not accion.isdigit() and accion not in ["1", "2", "3", "4", "5", "6", "7", "10"]:
+                print("El valor ingresado es incorrecto. Por favor ingrese solamente un número del menú")
+                accion = input("""
+                    Ingrese un número para elegir la actividad a realizar.
+                        1. Carga de herramientas.
+                        2. Carga de existencias.
+                        3. Visualizar el inventario.
+                        4. Consulta de stock por producto.
+                        5. Reporte de quiebre de stock.
+                        6. Alta de nuevo producto.
+                        7. Actualizacion de stock.
+                            Venta o compra.
+                        10. Para salir del programa. 
+                    """).strip()
         case "6":
-            pass
+            # Alta de nuevo producto
+            alta = input("Ingrese el producto nuevo ").strip().lower()
+            while not alta.isalpha():
+                alta = input("Ingrese solo letras para el producto nuevo ").strip().lower()
+
+            cant = input("Ingrese la cantidad ").strip()
+            while not cant.isdigit():
+                cant = input("Valor incorrecto para la cantidad, ingrese solo número mayor que 0 ")
+            cant = int(cant)
+            # isalpha me condiciona a que el numero de cant sea sin signo por lo tanto es positivo pero como en el 
+            # enunciado pide que valor de existencia negativo se lo trate igual que a nombre repetido o vacio lo 
+            # incluyo en el while y en el if 
+            while alta in herramientas or alta == "" or cant < 0:
+                if not alta.isalpha():
+                    print("Para la herramienta debe elegir solo letras")
+                elif alta in herramientas:
+                    print("La herramienta ya existe en la base de datos")
+                elif cant < 0:
+                    print("La cantidad no puede ser un número negativo")
+                else:
+                    print("El nombre de la herramienta no puede estar vacío")
+                print("Por favor elija nuevamente un número del menú")
+                accion = input("""
+                    Ingrese un número para elegir la actividad a realizar.
+                        1. Carga de herramientas.
+                        2. Carga de existencias.
+                        3. Visualizar el inventario.
+                        4. Consulta de stock por producto.
+                        5. Reporte de quiebre de stock.
+                        6. Alta de nuevo producto.
+                        7. Actualizacion de stock.
+                            Venta o compra.
+                        10. Para salir del programa. 
+                    """).strip()
+                break
+            herramientas.append(alta)
+            existencias.append(cant)
+             # Vuelvo a solicitar elección de menu 
+            accion = input("""
+                Ingrese un número para elegir la actividad a realizar.
+                    1. Carga de herramientas.
+                    2. Carga de existencias.
+                    3. Visualizar el inventario.
+                    4. Consulta de stock por producto.
+                    5. Reporte de quiebre de stock.
+                    6. Alta de nuevo producto.
+                    7. Actualizacion de stock.
+                        Venta o compra.
+                    10. Para salir del programa. 
+                """).strip()
+            
         case "7":
             pass
         case "10":
